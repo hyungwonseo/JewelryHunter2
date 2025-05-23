f02fbd2a35daa4d32b2319501b3c4531f7b95548
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-05-23 09:26:01 +0900

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rbody;
+    float axisH = 0.0f;
+    public float speed = 3.0f;
+
+    public float jump = 9.0f;
+    public LayerMask groundLayer;
+    bool goJump = false;
+    bool onGround = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        axisH = Input.GetAxisRaw("Horizontal");
+        if (axisH > 0.0f)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+        else if (axisH < 0.0f)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    // 
+    private void FixedUpdate()
+    {
+        // 그라운드에 서있는지 판정하는 코드
+        onGround = Physics2D.Linecast(transform.position,
+            transform.position - (transform.up * 0.1f),
+            groundLayer);
+
+        if (onGround || axisH != 0)
+        {
+            rbody.velocity = new Vector2(axisH * speed, rbody.velocity.y);
+            // y의 값은 여기서 조정하지않고 다른곳에서 적용하는 y벡터값을 그대로 사용하라는 뜻
+            // y는 기존값 유지!!
+        }
+
+        if (onGround && goJump)
+        {
+            rbody.AddForce(new Vector2(0, jump), ForceMode2D.Impulse);
+            goJump = false;
+        }
+    }
+    
+    public void Jump()
+    {
+        goJump = true;
+    }
+}
